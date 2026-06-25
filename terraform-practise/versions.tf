962d42755641c0eea58c07c4102873c12a3fc736
Filip <filip@laptop-1.home>
2026-06-25 07:21:45 +0200

    remote state - day 8

--- terraform-practise/versions.tf
+++ terraform-practise/versions.tf
@@ -6,4 +6,11 @@ terraform {
       version = ">= 4.0.0"
     }
   }
+    backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "sttfstatefilip7123" 
+    container_name       = "tfstate"
+    key                  = "practise.terraform.tfstate"
+    use_azuread_auth     = true
+  }
 }
