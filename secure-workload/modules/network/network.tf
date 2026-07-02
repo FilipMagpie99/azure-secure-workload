1aca2de171c97eae333adc2f53cfce415ea9b740
Filip <filip@laptop-2.home>
2026-07-02 19:39:06 +0200

    day 3 - linux web app (front/back end) MIs, VNET integration, apps hardening

--- secure-workload/modules/network/network.tf
+++ secure-workload/modules/network/network.tf
@@ -8,28 +8,35 @@ resource "azurerm_virtual_network" "vnet-secure-workload" {
 resource "azurerm_subnet" "snet-app-gw" {
   name                 = "snet-app-gw"
   resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnet_name
+  virtual_network_name = azurerm_virtual_network.vnet-secure-workload.name
   address_prefixes     = ["10.1.1.0/24"]
 }
 
 resource "azurerm_subnet" "snet-app" {
   name                 = "snet-app"
   resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnet_name
+  virtual_network_name = azurerm_virtual_network.vnet-secure-workload.name
   address_prefixes     = ["10.1.2.0/28"]
+  delegation {
+    name = "delegation-app-service"
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
 }
 
 resource "azurerm_subnet" "snet-data" {
   name                 = "snet-data"
   resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnet_name
+  virtual_network_name = azurerm_virtual_network.vnet-secure-workload.name
   address_prefixes     = ["10.1.3.0/28"]
 }
 
 resource "azurerm_subnet" "snet-pe" {
   name                 = "snet-pe"
   resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnet_name
+  virtual_network_name = azurerm_virtual_network.vnet-secure-workload.name
   address_prefixes     = ["10.1.4.0/28"]
 }
 
@@ -46,23 +53,23 @@ resource "azurerm_network_security_group" "nsg-data" {
 }
 
 resource "azurerm_application_security_group" "asg-front" {
-    name = "asg-front"
-    location = var.location
-    resource_group_name = var.resource_group_name
+  name                = "asg-front"
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_application_security_group" "asg-back" {
-    name = "asg-back"
-    location = var.location
-    resource_group_name = var.resource_group_name
+  name                = "asg-back"
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
 
-resource "azurerm_subnet_network_security_group_association" "snet-app-nsg-association"{
-    subnet_id = azurerm_subnet.snet-app.id
-    network_security_group_id = azurerm_network_security_group.nsg-app.id
+resource "azurerm_subnet_network_security_group_association" "snet-app-nsg-association" {
+  subnet_id                 = azurerm_subnet.snet-app.id
+  network_security_group_id = azurerm_network_security_group.nsg-app.id
 }
 
-resource "azurerm_subnet_network_security_group_association" "snet-data-nsg-association"{
-    subnet_id = azurerm_subnet.snet-data.id
-    network_security_group_id = azurerm_network_security_group.nsg-data.id
+resource "azurerm_subnet_network_security_group_association" "snet-data-nsg-association" {
+  subnet_id                 = azurerm_subnet.snet-data.id
+  network_security_group_id = azurerm_network_security_group.nsg-data.id
 }
---
+++ secure-workload/modules/network/outputs.tf
@@ -0,0 +1,6 @@
+output "subnet_id_app" {
+  value = azurerm_subnet.snet-app.id
+}
+output "subnet_id_pe" {
+  value = azurerm_subnet.snet-pe.id
+}
