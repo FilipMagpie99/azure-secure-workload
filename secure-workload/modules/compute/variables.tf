7cdbec404a66351fb6c6c75afbabaef96fbd72fa
Filip <filip@macbook-filip-2.home>
2026-08-11 12:33:45 +0200

    chore: repo cleanup, ADR renumbering, admin identity via secrets

--- secure-workload/modules/compute/variables.tf
+++ secure-workload/modules/compute/variables.tf
@@ -34,11 +34,9 @@ variable "dev_ip" {
 variable "pg_entra_admin_object_id" {
   description = "Object ID of the PostgreSQL Entra administrator"
   type        = string
-  default     = "cb1669fe-2094-4994-a233-61f0396b65ac"
 }
 
 variable "pg_entra_admin_upn" {
   description = "UPN of the PostgreSQL Entra administrator"
   type        = string
-  default     = "fsroczazur01_outlook.com#EXT#@fsroczazur01outlook.onmicrosoft.com"
 }
